084db6a6b86eb18d55518a431335ae8c9b5ff904
GiovanaCris <giovana.bp@outlook.com>
2025-05-23 16:51:58 -0300

    back

--- Formativa-PWBE-main/Formativa/projeto/projeto/settings.py
+++ Formativa-PWBE-main/Formativa/projeto/projeto/settings.py
@@ -66,7 +66,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheader.middleware.CorsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'projeto.urls'
